38bb5e566ce98a34f1b01579144983b1bf80897d
Victor Belikov <v1ctor.belikov@gmail.com>
2021-02-08 20:21:53 +0300

    Feat: add 'select' instead 'input'

--- lambda/handler.js
+++ lambda/handler.js
@@ -12,17 +12,11 @@ module.exports.saveInDb = async (event) => {
       const uuid = Date.now().toString();
       const channelType = 'SMS';
       const createdAt = new Date().toLocaleDateString();
-      let tableDB;
-
-      if (countryCode.toUpperCase() === 'BY') {
-        tableDB = TABLE_BY;
-      } else if (countryCode.toUpperCase() === 'UA') {
-        tableDB = TABLE_UA;
-      }
+      const tableDB = countryCode === 'BY' ? TABLE_BY : TABLE_UA;
 
       const consent = {
         TableName: tableDB,
-        Item: { uuid, channelType, channelValue: phoneNumber, countryCode: countryCode.toUpperCase(), createdAt },
+        Item: { uuid, channelType, channelValue: phoneNumber, countryCode: countryCode, createdAt },
       };
 
       await documentClient.put(consent).promise();
